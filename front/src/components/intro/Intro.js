4a67f43b50df5b28130023b7aa96129d3c043dce
songgaram <pager1436@gmail.com>
2022-04-22 23:11:59 +0900

    style : 백그라운드 이미지 삽입 / react-route-dom 설치

--- front/src/components/intro/Intro.js
+++ front/src/components/intro/Intro.js
@@ -1,5 +1,13 @@
+import "./Intro.css";
 const Intro = () => {
-  return;
+  return (
+    <div id="wrapper">
+      <span id="title">
+        I Do Like <br />
+        Board Games!
+      </span>
+    </div>
+  );
 };
 
 export default Intro;
