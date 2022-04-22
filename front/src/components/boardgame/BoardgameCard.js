61f53fa3482134f927fdf84ae13d3143bb114186
aldrn <aldrn29@gmail.com>
2022-04-22 23:27:44 +0900

    feat : boardgame 화면 구성에 필요한 컴포넌트(Boardgame, BoardgameCard, BoardgameCategory) 추가

---
+++ front/src/components/boardgame/BoardgameCard.js
@@ -0,0 +1,12 @@
+import './BoardgameCard.css'
+
+function BoardgameCard() {
+    return (
+        <div className='boardgame-card'>
+            <div className='boardgame-card-img'></div>
+            <div className='boardgame-card-contents'></div>
+        </div>
+    )
+}
+
+export default BoardgameCard
