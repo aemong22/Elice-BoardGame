66f059b0da0513c3fb055ed8a5ee430a95c023ec
nowgnas <marshmello.sw@gmail.com>
2022-04-23 15:32:37 +0900

    refact : refresh token 저장 방식 변경(create->update)

--- back/src/db/models/Token.js
+++ back/src/db/models/Token.js
@@ -1,14 +1,19 @@
 import { TokenModel } from "../schemas/token";
 
 class Token {
-  static async create({ newUserToken }) {
-    const token = await TokenModel.create(newUserToken);
-    return token;
-  }
-
   static async getToken({ user_id }) {
     const userToken = await TokenModel.findOne({ user_id });
     return userToken;
+  }
+
+  static async updateRefresh({ _id, refresh_token }) {
+    // _id와 refresh token update, 없으면 추가
+    const update = await TokenModel.updateOne(
+      { _id },
+      { _id, refresh_token },
+      { upsert: true }
+    );
+    return update;
   }
 }
 
