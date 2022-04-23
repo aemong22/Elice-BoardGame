c283a32917d9f05a70dd9659976636366f8c0fee
songgaram <pager1436@gmail.com>
2022-04-23 18:03:34 +0900

    feat : Home.js 로그인 유저인지 판별 기능 구현

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,24 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import * as Api from "../api";
-
 import Header from "./Header";
+import { useSelector } from "react-redux";
 
 function Home() {
-    const navigate = useNavigate();
-    const params = useParams();
+  const navigate = useNavigate();
+  const [isFetchCompleted, setIsFetchCompleted] = useState(false);
+  const userState = useSelector((state) => state.userReducer.user);
 
-    // 유저 로그인 상태 관련 코드 추가하기
+  // 만약 로그인 된 상태가 아니라면 intro 페이지로 이동시키기
+  useEffect(() => {
+    if (!userState) {
+      navigate("/intro", { replace: true });
+      return;
+    }
 
-    // 만약 로그인 된 상태가 아니라면 intro 페이지로 이동시키기
-    navigate("/intro", { replace: true });
+    setIsFetchCompleted(true);
+  }, [params, userState, navigate]);
 
-    return (
-        <>
-            <Header />
-            Home!
-        </>
-    )
+  if (!isFetchCompleted) {
+    return <div>로딩중...</div>;
+  }
+
+  return (
+    <>
+      <Header />
+      Home!
+    </>
+  );
 }
 
 export default Home;
