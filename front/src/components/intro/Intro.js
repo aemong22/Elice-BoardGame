0d3ea9c2da9f295ebcda3537b981809e291670ac
songgaram <pager1436@gmail.com>
2022-04-23 13:33:12 +0900

    style : 타이틀 / 서브 타이틀 위치, 컬러 설정

--- front/src/components/intro/Intro.js
+++ front/src/components/intro/Intro.js
@@ -1,11 +1,22 @@
 import "./Intro.css";
+// import RegisterBtn from "./RegisterBtn";
+
 const Intro = () => {
   return (
-    <div id="wrapper">
-      <span id="title">
-        I Do Like <br />
-        Board Games!
-      </span>
+    <div id="container">
+      <div id="wrapper">
+        <div id="wrapper_letter">
+          <span id="title">
+            I Do Like <br />
+            Board Games!
+          </span>
+          <span id="sub_title">
+            다양한 보드게임을 추천받고 원하는 게임을 선택해보세요!
+          </span>
+        </div>
+
+        {/* <RegisterBtn /> */}
+      </div>
     </div>
   );
 };
