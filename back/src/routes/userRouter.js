f89beb3b82eb63f31b789da12bd26cbd6d22d3a4
[4기-1반]김애림 <pinkpig_21@naver.com>
2022-04-23 12:45:42 +0900

    실행오류 : 해결

--- back/src/routes/userRouter.js
+++ back/src/routes/userRouter.js
@@ -1,6 +1,5 @@
 import is from "@sindresorhus/is";
 import { Router } from "express";
-import { login_required } from "../middlewares/login_required";
 import { userAuthService } from "../services/userService";
 import { refresh } from "../utils/refresh";
 import { authJWT } from "../middlewares/authJWT";
