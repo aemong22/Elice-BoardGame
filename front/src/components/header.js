2d3792b831fdb9dedcdd9b708ea8f17e64216b8a
songgaram <pager1436@gmail.com>
2022-04-25 16:35:09 +0900

    feat : 로그아웃 기능 구현

--- front/src/components/header.js
+++ front/src/components/header.js
@@ -1,22 +1,36 @@
 import { useNavigate } from "react-router-dom";
-import "./Header.css"
+import "./Header.css";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../store/actions/userAction";
 
 function Header() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    return (
-        <>
-            <div className="header">
-                <ul className="header-navigate">
-                    <li onClick={() => navigate("/")}>Home</li>
-                    <li onClick={() => navigate("/about")}>About</li>
-                    <li onClick={() => navigate("/boardgame")}>Boardgame</li>
-                    <li>MyPage</li>
-                    
-                </ul>
-            </div>
-        </>
-    )
+  const logout = () => {
+    // sessionStorage 에 저장했던 JWT 토큰을 삭제함.
+    sessionStorage.removeItem("userToken");
+    // dispatch 함수를 이용해 로그아웃함.
+    dispatch(logoutUser());
+    // 기본 페이지로 돌아감.
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div className="header">
+        <ul className="header-navigate">
+          <li onClick={() => navigate("/")}>Home</li>
+          <li onClick={() => navigate("/about")}>About</li>
+          <li onClick={() => navigate("/boardgame")}>Boardgame</li>
+          <li>MyPage</li>
+          <li onClick={logout} style={{ cursor: "pointer" }}>
+            Logout
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Header;
--- front/src/store/actions/userAction.js
+++ front/src/store/actions/userAction.js
@@ -1,8 +1,15 @@
-import { LOGIN_USER } from "./actionTypes";
+import { LOGIN_USER, LOGOUT_USER } from "./actionTypes";
 
 export const loginUser = (user) => {
   return {
     type: LOGIN_USER,
     user: user,
+  };
+};
+
+export const logoutUser = () => {
+  return {
+    type: LOGOUT_USER,
+    user: null,
   };
 };
