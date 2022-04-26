130982764f68ddc881d08f91a8771a468a1db138
nuri <noori1050@naver.com>
2022-04-26 15:20:43 +0900

    fix : Token 스키마 변수 수정
    - user_id에 unique key 속성 추가
    - 피드백 반영

--- back/src/db/schemas/token.js
+++ back/src/db/schemas/token.js
@@ -1,19 +1,20 @@
 import { Schema, model } from "mongoose";
 
 const TokenSchema = new Schema(
-  {
-    user_id: {
-      type: String,
-      required: true,
+    {
+        user_id: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        refresh_token: {
+            type: String,
+            required: true,
+        },
     },
-    refresh_token: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 const TokenModel = model("Token", TokenSchema);
