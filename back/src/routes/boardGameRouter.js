2d7cfc84ef6d9ce0e0a892a1583cdf2cd4847b80
nowgnas <marshmello.sw@gmail.com>
2022-04-26 16:45:22 +0900

    docs : swagger에 board game 플레이어수로 조회 추가

--- back/src/routes/boardGameRouter.js
+++ back/src/routes/boardGameRouter.js
@@ -2,6 +2,7 @@ import { boardGameService } from "../services/boardGameService";
 import { Router } from "express";
 
 const boardGameRouter = Router();
+// board game 라우팅도 authJWT는 추가할 것 - 현재는 테스트를 위해 추가하지 않음
 
 boardGameRouter.get("/boardGames", async (req, res, next) => {
   try {
