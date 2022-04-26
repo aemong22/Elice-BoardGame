5a76e0aa7c5991eb2da287acc32a87b0c995a7bf
nowgnas <marshmello.sw@gmail.com>
2022-04-27 02:07:24 +0900

    feat : add recently board game

--- back/src/mock.js
+++ back/src/mock.js
@@ -4,39 +4,38 @@ import { Boardame } from "./db/models/BoardGame";
 
 const insertData = (req, res, next) => {
     fs.readFile(
-        "/Users/nowgnas/projects/board-game-recommendation-project/back/src/dataset.json",
+        "/Users/nowgnas/projects/board-game-recommendation-project/back/src/recently.json",
         (err, json) => {
             if (err) {
                 return console.log(err);
             }
             const jsonData = JSON.parse(json);
             jsonData.data.forEach(async (element) => {
+                console.log(element.index);
                 const item = {
                     index: element.index,
-                    unNamed: element["Unnamed: 0"],
-                    gameId: element.ID,
-                    gameName: element.Name,
+                    un_named: element["Unnamed: 0"],
+                    game_id: element.ID,
+                    game_name: element.Name,
                     year: element.Year,
                     rank: element.Rank,
-                    averageRating: element.Average,
-                    bayesAverage: element["Bayes average"],
-                    userRated: element["Users rated"],
+                    average_rating: element.Average,
+                    bayes_average: element["Bayes average"],
+                    user_rated: element["Users rated"],
                     url: element.URL,
                     thumbnail: element.Thumbnail,
-                    minPlayer: element.minplayers,
-                    maxPlayer: element.maxplayers,
-                    minAge: element.minage,
-                    playingTime: element.playingtime,
-                    minPlayingTime: element.minplaytime,
-                    maxPlayingTime: element.maxplaytime,
-                    complexityAverage: element["Complexity Average"],
+                    min_player: element.minplayers,
+                    max_player: element.maxplayers,
+                    min_age: element.minage,
+                    playing_time: element.playingtime,
+                    min_playing_time: element.minplaytime,
+                    max_playing_time: element.maxplaytime,
+                    complexity_average: element["Complexity Average"],
                     domains: element.Domains,
                     image: element.image,
                     description: element.description,
-                    recommendId: element.RecommendID,
                 };
                 const insertData = await Boardame.insert({ item });
-                console.log(insertData);
             });
         }
     );
--- back/src/services/boardGameService.js
+++ back/src/services/boardGameService.js
@@ -10,6 +10,34 @@ class boardGameService {
         const games = await Boardame.findByPlayer({ player });
         return games;
     }
+
+    static async getGameByCondition({ category, sortType }) {
+        // 인원수 player
+        // 연령 age
+        // 테마 theme
+        // 게임시간 time
+        // 난이도 complexity
+
+        // 랭킹 ranking
+        // 리뷰 review
+        // 평점 rating
+        switch (category) {
+            case player:
+                break;
+            case age:
+                break;
+            case theme:
+                break;
+            case time:
+                break;
+            case complexity:
+                break;
+            case null:
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 export { boardGameService };
