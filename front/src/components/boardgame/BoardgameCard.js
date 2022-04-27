112f8802aa6d196487a563c1da16de51c94ebc46
aldrn <aldrn29@gmail.com>
2022-04-28 04:32:28 +0900

    style : 보드게임 카드에 나타낼 정보 디자인 추가

--- front/src/components/boardgame/BoardgameCard.js
+++ front/src/components/boardgame/BoardgameCard.js
@@ -12,42 +12,41 @@ import Button from '@mui/material/Button';
 // @mui/icons-material 설치
 function BoardgameCard({ name, min_player, max_player, domains, image }) {
 
-  const domainList = domains.split(',')
+  const maxLength = 13;
+  const boardgameName = name.length > maxLength ? name.substr(0, maxLength) + '...' : name;
+  const domainList = domains.split(',');
   
   return (
     <Card sx={{ width: 250, maxWidth: 270, my: 10 }}>
       <CardActionArea>
         <CardMedia
           component="img"
-          height="140"
-          maxHeight="140"
+          height="240"
           image={image}
           alt={name}
           sx={{ background: 'gray'}}
         />
         <CardContent sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
           <Typography gutterBottom variant="h6" component="span" sx={{ m: 'auto 0' }}>
-            {name}
+            { boardgameName }
           </Typography>
           <CardActions disableSpacing>
             <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
+              <FavoriteIcon />
             </IconButton>
           </CardActions>
           <Typography component="div" sx={{ width: 100, height: 0 }}></Typography>
-
           <div>
             <Button variant="outlined" disabled sx={{ fontSize: '0.5rem', borderRadius: '100px', m: '2px' }}>
               {min_player}~{max_player}인용
             </Button>
             {domainList.map((domain) => (
-              <Button variant="outlined" disabled sx={{ fontSize: '0.5rem', borderRadius: '100px', m: '2px' }}>
+              <Button key={domain} variant="outlined" disabled sx={{ fontSize: '0.5rem', borderRadius: '100px', m: '2px' }}>
                 {domain}
               </Button>
             ))}
           </div>
         </CardContent>
-
       </CardActionArea>
     </Card>
   );
