5a8150a4cfaf4d2b65a74aeb982c764375dc0bfb
songgaram <pager1436@gmail.com>
2022-04-28 02:11:44 +0900

    feat : detail 컴포넌트 내용 추가
    400여종 보드게임 선택 부분

--- front/src/components/home/Detail.js
+++ front/src/components/home/Detail.js
@@ -2,14 +2,24 @@ import "./Detail.css";
 
 const Detail = () => {
   return (
-    <div id="detail_wrapper">
-      <div id="detail_title">친구끼리 보드게임</div>
-      <img
-        alt="친구끼리 보드게임"
-        src="/image/detail_visual.png"
-        style={{ width: "30%" }}
-      />
-    </div>
+    <>
+      <div id="detail_wrapper_top">
+        <div className="detail_title">원하는 인원수로</div>
+        <img
+          alt="친구끼리 보드게임"
+          src="/image/detail_visual.png"
+          style={{ width: "30%" }}
+        />
+      </div>
+      <div id="detail_wrapper_middle">
+        <img
+          alt="보드게임 선택"
+          src="/image/detail_visual2.png"
+          style={{ width: "40%" }}
+        />
+        <div className="detail_title">400여종의 보드게임</div>
+      </div>
+    </>
   );
 };
 
