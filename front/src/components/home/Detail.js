4c2bfda7eb982ff31223cc6438145407c770aa1c
songgaram <pager1436@gmail.com>
2022-04-27 23:57:28 +0900

    feat : detail 컴포넌트 내용 추가
    친구들과 보드게임 이미지 삽입

--- front/src/components/home/Detail.js
+++ front/src/components/home/Detail.js
@@ -1,7 +1,16 @@
 import "./Detail.css";
 
 const Detail = () => {
-  return;
+  return (
+    <div id="detail_wrapper">
+      <div id="detail_title">친구끼리 보드게임</div>
+      <img
+        alt="친구끼리 보드게임"
+        src="/image/detail_visual.png"
+        style={{ width: "30%" }}
+      />
+    </div>
+  );
 };
 
 export default Detail;
