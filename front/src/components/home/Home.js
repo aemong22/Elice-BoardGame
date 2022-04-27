1e3df3e1f6630996e68e4adeaaee4dbb90546177
songgaram <pager1436@gmail.com>
2022-04-27 14:57:00 +0900

    feat : app.js 비동기 통신 수정
    - useEffect에 넣어줬음
    - dispatch로 배열에 넣어줬음

--- front/src/components/home/Home.js
+++ front/src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Header from "./Header";
+import Header from "../Header";
 import { useSelector } from "react-redux";
 import "./Home.css";
 
