e41d84da27a0fdcb400078579a37097a790b52ae
nowgnas <marshmello.sw@gmail.com>
2022-04-27 23:58:05 +0900

    refact : .env 환경 추가

--- back/index.js
+++ back/index.js
@@ -2,7 +2,6 @@ import { app } from "./src/app";
 import "dotenv/config";
 
 // .env로 적용 안돼서 config 파일로 대체
-// const PORT = process.env.SERVER_PORT || 5001;
 const PORT = process.env.SERVER_PORT || 5001;
 
 app.listen(PORT, () => {
