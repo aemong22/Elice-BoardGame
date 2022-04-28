dda69a1a30f53a4510a4181e2d92652aa8e6f7b5
aldrn <aldrn29@gmail.com>
2022-04-28 16:16:03 +0900

    fix : 보드게임 useState 이름 수정

--- front/src/components/boardgame/Boardgame.js
+++ front/src/components/boardgame/Boardgame.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import BoardgameCategory from "./BoardgameCategory"
-import Header from "../Header";
+import BoardgameCategory from "./BoardgameCategory";
 import BoardgameData from "./BoardgameData";
-import "./Boardgame.css"
+import Header from "../Header";
+import "./Boardgame.css";
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchField from "./SearchField";
 
 function Boardgame() {
-    const [openCategory, setOpenCategory] = useState(false);
+    const [open, setOpen] = React.useState(false);
 
     // 필터에 사용할 변수
     const [player, setPlayer] = useState([]);
@@ -19,14 +19,14 @@ function Boardgame() {
                 <Header />
             </div>
             <div className='boardgame-header'>
-                <div className='boardgame-header-item' onClick={() => setOpenCategory(!openCategory) }>
+                <div className='boardgame-header-item' onClick={() => setOpen(true) }>
                     <MenuIcon className='boardgame-header-menu-icon'></MenuIcon>
                     <div className='boardgame-header-menu'>Menu</div>
                 </div>
                 <SearchField />
             </div>
             <div className='boardgame-container'>
-                <div className={ openCategory ? 'boardgame-category active' : 'boardgame-category' }>
+                <div className={ open ? 'boardgame-category active' : 'boardgame-category' }>
                     <BoardgameCategory 
                         player={player}
                         setPlayer={setPlayer}
@@ -35,14 +35,6 @@ function Boardgame() {
                     />
                 </div>
                 <div className='boardgame-wrapper'>
-                    {/* <div className='boardgame-sort'>
-                        <div className='boardgame-filterd'>인원수</div>
-                        <div className='boardgame-sort-items'>
-                            <li>랭킹순</li>
-                            <li>리뷰순</li>
-                            <li>평점순</li>
-                        </div>
-                    </div> */}
                     <div className='boardgames'>
                         <BoardgameData
                             player={player}
