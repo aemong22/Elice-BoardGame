6df139058b9ace5820a6d10257cd43b0cdfccfde
aldrn <aldrn29@gmail.com>
2022-04-29 20:57:59 +0900

    feat : 보드게임 Pagenation 기능 추가

--- front/src/components/boardgame/Boardgame.js
+++ front/src/components/boardgame/Boardgame.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BoardgameCategory from "./BoardgameCategory";
 import BoardgameData from "./BoardgameData";
 import Header from "../Header";
@@ -13,35 +13,40 @@ function Boardgame() {
     const [player, setPlayer] = useState([]);
     const [category, setCategory] = useState([]);
 
+    useEffect(() => {
+        // test
+        console.log(player);
+        console.log(category);
+      }, [player, category]);
+
     return (
         <>
             <div className='header'>
                 <Header />
             </div>
             <div className='boardgame-header'>
-                <div className='boardgame-header-item' onClick={() => setOpen(true) }>
+                <div className='boardgame-header-item' onClick={() => setOpen(!open) }>
                     <MenuIcon className='boardgame-header-menu-icon'></MenuIcon>
                     <div className='boardgame-header-menu'>Menu</div>
                 </div>
                 <SearchField />
             </div>
             <div className='boardgame-container'>
-                <div className={ open ? 'boardgame-category active' : 'boardgame-category' }>
-                    <BoardgameCategory 
+                <div className='boardgames'>
+                    <BoardgameData
                         player={player}
-                        setPlayer={setPlayer}
                         category={category}
-                        setCategory={setCategory}
                     />
                 </div>
-                <div className='boardgame-wrapper'>
-                    <div className='boardgames'>
-                        <BoardgameData
-                            player={player}
-                            category={category}
-                        />
-                    </div>
-                </div>
+            </div>
+
+            <div className={ open ? 'boardgame-category active' : 'boardgame-category' }>
+                <BoardgameCategory 
+                    player={player}
+                    setPlayer={setPlayer}
+                    category={category}
+                    setCategory={setCategory}
+                />
             </div>
         </>
     )
--- front/src/components/boardgame/BoardgameData.js
+++ front/src/components/boardgame/BoardgameData.js
@@ -1,31 +1,80 @@
 import React, { useState, useEffect } from "react";
-import BoardgameCard from "./BoardgameCard"
+import BoardgameCard from "./BoardgameCard";
+import BasicPagination from "./BasicPagination";
 import * as Api from "../../api";
 
 function BoardgameData({ player, category }) {
     const [boardgames, setBoardgames] = useState([]);
 
-    // 보드게임 전체 데이터 불러오기
-    useEffect(() => {
-        try {
-            Api.get("boardGames").then((res) => {
-                setBoardgames(res.data);
-            })
-        } catch (err) {
-            console.log("errer message: ", err)
-        }
-    }, []);
+    const [limit, setLimit] = useState(9);
+    const [page, setPage] = useState(1);
+    const offset = (page - 1) * limit;
 
-    // 보드게임 플레이어 수로 데이터 불러오기
+    // 보드게임 전체 데이터 불러오기
     // useEffect(() => {
     //     try {
-    //         Api.get("games/player", player[0]).then((res) => {
+    //         Api.get("boardGames").then((res) => {
     //             setBoardgames(res.data);
     //         })
     //     } catch (err) {
     //         console.log("errer message: ", err)
     //     }
-    // }, [player]);
+    // }, []);
+
+
+    /*
+    카테고리 : 인원수 / 연령 / 테마 / 게임시간 / 난이도
+
+    정렬 : 랭킹순 / 평점순 / 리뷰순
+
+    인원수 : 2 / 3 / 4 / 5 이상
+    연령 : 9 / 12 / 15 / 19  
+    난이도 : 1 ~ 5
+    테마(domain) : Thematic / strategy /  family / customizable ...
+    게임시간 (minplaytime) :  30 / 60 / 120~
+
+
+    post("boardgame") {
+players : 2 || "",
+time :234 || "",
+...
+sort : sorting || ""
+}
+
+const [sort, setSort] = useState("")
+
+useEffect(()=>{
+  Api.post('router', {
+  players: player,
+  time: time,
+   ... , 
+  sort: sort,
+  })
+},[players,time, ... , sort])
+
+
+<button
+ value="rank"
+ onClick={(e)=> setSort(e.target.value)}
+ >rank</button>
+ 
+ <button
+ value="review"
+ onClick={(e)=> setSort(e.target.value)}
+ >review</button>
+    */
+
+
+    // 보드게임 플레이어 수로 데이터 불러오기
+    useEffect(() => {
+        try {
+            Api.get("games", player[0]).then((res) => {
+                setBoardgames(res.data);
+            })
+        } catch (err) {
+            console.log("errer message: ", err)
+        }
+    }, [player]);
 
     const boardgameList = boardgames.map((boardgame) => (
         <BoardgameCard
@@ -40,7 +89,31 @@ function BoardgameData({ player, category }) {
 
     return (
         <>
-            {boardgameList}
+            {boardgameList.slice(offset, offset + limit)}
+
+            {/* 페이지네이션 */}
+            <div style={{ width: '100%', display: 'flex' }}>
+                <div style={{ margin: '0 auto', display: 'flex', alignItems: 'center' }}>
+                <BasicPagination 
+                    total={boardgameList.length}
+                    limit={limit}
+                    page={page}
+                    setPage={setPage}
+                />
+                <label>
+                    <select
+                        type="number"
+                        value={limit}
+                        onChange={({ target: { value }}) => setLimit(Number(value))}
+                    >
+                        <option value="9">9</option>
+                        <option value="15">15</option>
+                        <option value="30">30</option>
+                        <option value="90">90</option>
+                    </select>
+                </label>
+                </div>
+            </div>
         </>
     )
 }
