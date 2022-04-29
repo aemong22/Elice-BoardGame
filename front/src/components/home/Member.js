ae1a871d83ee581f890acf9cc1713ef96c452d7c
songgaram <pager1436@gmail.com>
2022-04-29 18:38:28 +0900

    style : background 이미지 추가 / span 태그 생성

--- front/src/components/home/Member.js
+++ front/src/components/home/Member.js
@@ -1,5 +1,22 @@
+import "./Member.css";
+
 function Member() {
-  return;
+  return (
+    <div className="member-container">
+      <div className="member-background">
+        <div className="member-1st-floor">
+          <span id="member-1">span</span>
+          <span id="member-2">span</span>
+          <span id="member-3">span</span>
+        </div>
+        <div className="member-2nd-floor">
+          <span id="member-4">span</span>
+          <span id="member-5">span</span>
+          <span id="member-6">span</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Member;
