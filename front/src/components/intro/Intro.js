64fa07a5782186e4b7554bdcb8262c5b75614f26
nuri <noori1050@naver.com>
2022-04-29 17:52:39 +0900

    fix : 필요없는 코드 삭제

--- front/src/components/intro/Intro.js
+++ front/src/components/intro/Intro.js
@@ -25,9 +25,6 @@ const Intro = () => {
                     <span id="sub_title">
                         다양한 보드게임을 추천받고 원하는 게임을 선택해보세요!
                     </span>
-                    <a href="https://accounts.google.com/o/oauth2/auth?client_id=74651602496-cli9c5rhrpb72fs07gvsramgr43dlu4o.apps.googleusercontent.com&redirect_uri=http://localhost:3000/oauth&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile">
-                        OAuth
-                    </a>
                 </div>
                 <div>
                     <RegisterBtn handleOpen={handleOpen} />
