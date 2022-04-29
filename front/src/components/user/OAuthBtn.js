496d60a8c3203f8d460306f6ce0c53471d347d96
nuri <noori1050@naver.com>
2022-04-29 20:57:44 +0900

    refact : google client_id .env 추가

--- front/src/components/user/OAuthBtn.js
+++ front/src/components/user/OAuthBtn.js
@@ -10,7 +10,7 @@ const OAuthBtn = () => {
         let url =
             protocol + "//" + hostName + (port ? ":" + port : "") + "/oauth";
         window.location.href =
-            "https://accounts.google.com/o/oauth2/auth?client_id=74651602496-cli9c5rhrpb72fs07gvsramgr43dlu4o.apps.googleusercontent.com&redirect_uri=" +
+            `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=` +
             url +
             "&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
     };
