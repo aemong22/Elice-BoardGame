4322c89cf455a5300d31a28620b17e834e9cb069
RamG <ramg@Ramgui-M1-pro.local>
2022-05-01 16:04:31 +0900

    content생성하기

--- back/src/controllers/boardController.js
+++ back/src/controllers/boardController.js
@@ -8,6 +8,25 @@ class boardServiceController{
         } catch (error) {
             next(error);
         }
+    }
+
+    static async createContents(req, res, next){
+        try {
+            const author = req.body.user_name;
+            const title = req.body.email;
+            const content = req.body.password;
+            const comment = [];
+
+            const newContent = await boardService.addContent({
+                author,
+                title,
+                content,
+                comment,
+            });
+            res.status(200).json(newContent);
+        } catch (error) {
+            next(error);
+        }
 }
 
 export {boardServiceController}
