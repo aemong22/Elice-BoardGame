88ab5c746791817931c05aa302b0fee04dced43a
aldrn <aldrn29@gmail.com>
2022-05-02 23:53:04 +0900

    fix : 보드게임 get 요청 시 사용할 api 추가(테스트)

--- front/src/api.js
+++ front/src/api.js
@@ -4,6 +4,7 @@ const backendPortNumber = "5001";
 const serverUrl =
   "http://" + window.location.hostname + ":" + backendPortNumber + "/";
 
+/*
 async function get(endpoint, params = "") {
   console.log(
     `%cGET 요청 ${serverUrl + endpoint + "/" + params}`,
@@ -11,6 +12,20 @@ async function get(endpoint, params = "") {
   );
 
   return axios.get(serverUrl + endpoint + "/" + params, {
+    // JWT 토큰을 헤더에 담아 백엔드 서버에 보냄.
+    headers: {
+      Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
+    },
+  });
+}*/
+
+async function get(endpoint, params = "") {
+  console.log(
+    `%cGET 요청 ${serverUrl + endpoint + "?" + params}`,
+    "color: #a25cd1;"
+  );
+
+  return axios.get(serverUrl + endpoint, params, {
     // JWT 토큰을 헤더에 담아 백엔드 서버에 보냄.
     headers: {
       Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
