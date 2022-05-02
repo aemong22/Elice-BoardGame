e3219d2cef6b2928ab88f47f70aebcfe552626d3
RamG <ramg@Ramgui-M1-pro.local>
2022-05-02 23:47:30 +0900

    board swagger작성

--- back/src/db/schemas/boardcontents.js
+++ back/src/db/schemas/boardcontents.js
@@ -17,7 +17,7 @@ const BoardContentSchema = new Schema(
             type : String,
             required: true,
         },
-        // comment 한 개시글에 여러개 연결
+        // comment 한 개시글에 여러개 연결›
         // 배열로 설정하는데 다른스키마와 연결시켜 작성자를 제공해주어야한다.
         comment :
             [{type : Schema.Types.ObjectId, ref : "BoardComment"}],
