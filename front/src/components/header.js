ab01394419d3d7017aea0fda0d2d79b64b47efbe
songgaram <pager1436@gmail.com>
2022-05-02 15:21:45 +0900

    refact : 필요없는 코드 삭제

--- front/src/components/header.js
+++ front/src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../store/actions/userAction";
