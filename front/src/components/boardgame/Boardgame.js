4620965874ea3ddd0c4546be85be2840844e0cfd
nuri <noori1050@naver.com>
2022-05-04 18:21:21 +0900

    fix : front 코드 수정

--- front/src/components/boardgame/Boardgame.js
+++ front/src/components/boardgame/Boardgame.js
@@ -2,87 +2,97 @@ import React, { useState } from "react";
 import BoardgameCategory from "./BoardgameCategory";
 import Sorting from "./Sorting";
 import BoardgameData from "./BoardgameData";
+import { categoryName, categoryValue } from "./BoardgameCategoryData";
 import "./Boardgame.css";
-import MenuIcon from "@mui/icons-material/Menu";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
 import SearchField from "./SearchField";
 
 function Boardgame() {
-  const header = document.getElementsByClassName("header");
-  const [open, setOpen] = useState(false);
-  const [condition, setCondition] = useState({
-    category: "",
-    val1: "",
-    type: "",
-    page: 1,
-    perPage: 9,
-  });
-
-  const initializeCondition = () => {
-    setCondition((current) => {
-      let newCondition = { ...current };
-      newCondition.category = "";
-      newCondition.val1 = "";
-      newCondition.type = "";
-      return newCondition;
+    const header = document.getElementsByClassName("header");
+    const [open, setOpen] = useState(false);
+    const [condition, setCondition] = useState({
+        category: "",
+        val1: "",
+        type: "",
+        page: 1,
+        perPage: 9,
     });
-  };
 
-  const changeCondition = (key, value) => {
-    setCondition((current) => {
-      let newCondition = { ...current };
-      newCondition[key] = value;
-      return newCondition;
-    });
-  };
+    const initializeCondition = () => {
+        setCondition((current) => {
+            let newCondition = { ...current };
+            newCondition.category = "";
+            newCondition.val1 = "";
+            newCondition.type = "";
+            return newCondition;
+        });
+    };
 
-  const changeOpen = () => {
-    setOpen(!open);
-  };
+    const changeCondition = (key, value) => {
+        setCondition((current) => {
+            let newCondition = { ...current };
+            newCondition[key] = value;
+            return newCondition;
+        });
+    };
 
-  return (
-    <>
-      <div
-        className={open ? "boardgame-container-slide" : "boardgame-container"}
-      >
-        {open
-          ? header[0].classList.add("boardgame-container-slide")
-          : header[0].classList.remove("boardgame-container-slide")}
-        <div className="boardgame-header">
-          <div className="boardgame-header-item" onClick={() => setOpen(!open)}>
-            <FilterAltOutlinedIcon className="boardgame-header-filtered-icon" />
-            <div className="boardgame-header-filtered">
-              {condition.category !== ""
-                ? `${categoryName(condition.category)} > ${categoryValue(
-                    condition.category,
-                    condition.val1
-                  )}`
-                : "최신 보드게임 보기"}
+    const changeOpen = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <>
+            <div
+                className={
+                    open ? "boardgame-container-slide" : "boardgame-container"
+                }
+            >
+                {open
+                    ? header[0].classList.add("boardgame-container-slide")
+                    : header[0].classList.remove("boardgame-container-slide")}
+                <div className="boardgame-header">
+                    <div
+                        className="boardgame-header-item"
+                        onClick={() => setOpen(!open)}
+                    >
+                        <FilterAltOutlinedIcon className="boardgame-header-filtered-icon" />
+                        <div className="boardgame-header-filtered">
+                            {condition.category !== ""
+                                ? `${categoryName(
+                                      condition.category
+                                  )} > ${categoryValue(
+                                      condition.category,
+                                      condition.val1
+                                  )}`
+                                : "최신 보드게임 보기"}
+                        </div>
+                    </div>
+                    <SearchField />
+                </div>
+                <div className="boardgame-sort">
+                    <Sorting changeCondition={changeCondition} />
+                </div>
+                <div className="boardgames">
+                    <BoardgameData
+                        condition={condition}
+                        changeCondition={changeCondition}
+                    />
+                </div>
             </div>
-          </div>
-          <SearchField />
-        </div>
-        <div className="boardgame-sort">
-          <Sorting changeCondition={changeCondition} />
-        </div>
-        <div className="boardgames">
-          <BoardgameData
-            condition={condition}
-            changeCondition={changeCondition}
-          />
-        </div>
-      </div>
-      <div
-        className={open ? "boardgame-category active" : "boardgame-category"}
-      >
-        <BoardgameCategory
-          condition={condition}
-          initializeCondition={initializeCondition}
-          changeCondition={changeCondition}
-          changeOpen={changeOpen}
-        />
-      </div>
-    </>
-  );
+            <div
+                className={
+                    open ? "boardgame-category active" : "boardgame-category"
+                }
+            >
+                <BoardgameCategory
+                    condition={condition}
+                    initializeCondition={initializeCondition}
+                    changeCondition={changeCondition}
+                    changeOpen={changeOpen}
+                />
+            </div>
+        </>
+    );
 }
 
 export default Boardgame;
