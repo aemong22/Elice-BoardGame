625848e9a41ccd02b11880b91317bc75fd09b0c0
songgaram <pager1436@gmail.com>
2022-05-05 01:38:32 +0900

    feat : coverflow에 이미지 삽입

--- front/src/components/boardgamedetail/BoardgameRec.js
+++ front/src/components/boardgamedetail/BoardgameRec.js
@@ -1,65 +1,39 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Coverflow from "react-coverflow";
 
-const fn = function () {
-    /* do your action */
-};
-
 function BoardgameRec({ recommendData }) {
-    console.log(recommendData);
+    const navigate = useNavigate();
+
+    const recommendGames = recommendData?.map((data) => (
+        <div
+            onClick={() => navigate(`/boardgame/detail/${data.game_id}`)}
+            key={data.game_id}
+            role="menuitem"
+            tabIndex="2"
+        >
+            <img
+                src={data.image}
+                alt={data.game_name}
+                style={{
+                    display: "block",
+                    width: "100%",
+                }}
+            />
+        </div>
+    ));
 
     return (
         <Coverflow
             width="100%"
-            height="300"
+            height="250"
             displayQuantityOfSide={2}
             navigation={false}
             enableScroll={true}
             clickable={true}
             active={0}
         >
-            {/* <Card sx={{ width: 50, my: 10, height: 100 }}>
-        <CardMedia component="img" image="/image/garam_pic.png" alt="garam" />
-        <CardContent>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Typography gutterBottom variant="h6" component="span">
-              보드게임이름
-            </Typography>
-          </div>
-        </CardContent>
-      </Card> */}
-
-            <div
-                onClick={() => fn()}
-                onKeyDown={() => fn()}
-                role="menuitem"
-                tabIndex="0"
-            >
-                <img
-                    src="/image/aerim_pic.png"
-                    alt="title or description"
-                    style={{
-                        display: "block",
-                        width: "100%",
-                    }}
-                />
-            </div>
-            <img
-                src="image/path"
-                alt="title or description"
-                data-action="http://andyyou.github.io/react-coverflow/"
-            />
-            <img
-                src="image/path"
-                alt="title or description"
-                data-action="http://andyyou.github.io/react-coverflow/"
-            />
+            {recommendGames}
         </Coverflow>
     );
 }
