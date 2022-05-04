9b3186ea13ff2db819c70fd2aef0801f0821133d
nuri <noori1050@naver.com>
2022-05-04 10:34:43 +0900

    refact : 변수명 수정

--- back/src/services/favoriteService.js
+++ back/src/services/favoriteService.js
@@ -1,5 +1,3 @@
-import bcrypt from "bcrypt";
-import { sign, refresh } from "../utils/jwt-utils";
 import { FavoriteModel } from "../db/schemas/favorite";
 import { UserModel } from "../db/schemas/user";
 import { BoardGameModel } from "../db/schemas/boardgame";
@@ -7,7 +5,7 @@ import { BoardGameModel } from "../db/schemas/boardgame";
 class favoriteAuthService {
     static async addFavorite({ userId, boardgameId }) {
         const user = await UserModel.findOne({ _id: userId });
-        const game = await BoardGameModel.findOne({ _id: boardgameId });
+        const game = await BoardGameModel.findOne({ game_id: boardgameId });
 
         if (!user) {
             const errorMessage = "해당 메일은 가입 내역이 없습니다.";
