1422c27f0ff7312d4d12a1797e2f243752798493
songgaram <pager1436@gmail.com>
2022-05-05 16:05:54 +0900

    feat : content map 으로 뿌려주기 / 글쓰기 아이콘 추가 완료

--- front/src/components/community/Community.js
+++ front/src/components/community/Community.js
@@ -10,6 +10,8 @@ import {
     CssBaseline,
     Divider,
 } from "@mui/material/";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import * as Api from "../../api";
 
 function community() {
     const navigate = useNavigate();
@@ -21,13 +23,14 @@ function community() {
             const res = await Api.get("communitycontents");
             setAllContents(res.data);
             setIsFetchCompleted(true);
+            console.log(res.data);
         } catch (error) {
             console.log(error);
         }
     };
 
     useEffect(() => {
-        fetchPostsInfo();
+        fetchContentsInfo();
     }, []);
 
     if (!isFetchCompleted) {
@@ -41,7 +44,6 @@ function community() {
                 <Box
                     sx={{
                         width: "100%",
-                        bgcolor: "#cfe8fc",
                         height: "100vh",
                         marginTop: "60px",
                     }}
@@ -62,10 +64,15 @@ function community() {
                                 display: "flex",
                                 flexDirection: "row",
                                 justifyContent: "space-between",
+                                alignItems: "center",
                             }}
                         >
                             <span>자유게시판</span>
-                            <span>글쓰기</span>
+                            <BorderColorIcon
+                                onClick={() =>
+                                    navigate("/communitycontents/create")
+                                }
+                            />
                         </ListSubheader>
                         <ListSubheader
                             component="div"
@@ -85,7 +92,7 @@ function community() {
                             <span>날짜</span>
                         </ListSubheader>
                         <Divider />
-                        {/* {allContents?.map((content, idx) => (
+                        {allContents?.map((content, idx) => (
                             <ListItemButton style={{ width: "100%" }}>
                                 <ListItemText
                                     secondary={idx}
@@ -100,94 +107,16 @@ function community() {
                                     style={{ textAlign: "center" }}
                                 />
                                 <ListItemText
-                                    secondary="날짜"
+                                    secondary={content.createdAt.slice(0, 10)}
                                     style={{ textAlign: "end" }}
                                 />
                             </ListItemButton>
-                        ))} */}
+                        ))}
                     </List>
                 </Box>
             </Container>
         </React.Fragment>
     );
 }
-
-// function community() {
-//     const navigate = useNavigate();
-//     const userState = useContext(UserStateContext);
-//     const [allPosts, setAllPosts] = useState([]);
-//     const [myPosts, setMyPosts] = useState([]);
-//     const [checked, setChecked] = useState(false);
-//     const [isFetchCompleted, setIsFetchCompleted] = useState(false);
-
-//     const fetchPostsInfo = async () => {
-//         try {
-//             //   const { data: tempAllPosts } = await Api.get('freeboardlist');
-//             //   setAllPosts(tempAllPosts);
-//             //   const { data: tempMyPosts } = await Api.get('freeboardlist', userState.user?.id);
-//             //   setMyPosts(tempMyPosts);
-//             //   setIsFetchCompleted(true);
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     };
-
-//     //   useEffect(() => {
-//     //     if (!userState.user) {
-//     //       navigate('/login');
-//     //       return;
-//     //     }
-//     //     fetchPostsInfo();
-//     //   }, [userState, navigate]);
-
-//     const toggleCheck = () => {
-//         setChecked(!checked);
-//     };
-
-//     if (!isFetchCompleted) {
-//         return <FreeBoardSkeleton />;
-//     }
-//     return (
-//         <Container style={{ position: "relative", minHeight: "100vh" }}>
-//             <Container className="mt-3">
-//                 <ToggleButton
-//                     className="mb-2"
-//                     id="toggle-check"
-//                     type="checkbox"
-//                     variant="outline-primary"
-//                     checked={checked}
-//                     onChange={toggleCheck}
-//                 >
-//                     {checked ? "모든 게시글 보기" : "내가 쓴 게시글 보기"}
-//                 </ToggleButton>
-//                 <Button
-//                     variant="primary"
-//                     style={{ position: "absolute", right: 13 }}
-//                     onClick={() => navigate(`/freeboard/create`)}
-//                 >
-//                     게시글 작성
-//                 </Button>
-//             </Container>
-//             <Card className="mt-4">
-//                 <Card.Header className="text-center">자유게시판</Card.Header>
-//                 <ListGroup   p variant="flush">
-//                     {(checked ? myPosts : allPosts).map((post) => (
-//                         <ListGroup.Item
-//                             key={post._id}
-//                             onClick={() => navigate(`/freeboard/${post._id}`)}
-//                         >
-//                             <Row style={{ cursor: "pointer" }}>
-//                                 <Col md={4}>{post.title}</Col>{" "}
-//                                 <Col md={{ span: 4, offset: 4 }}>
-//                                     {post.name}
-//                                 </Col>
-//                             </Row>
-//                         </ListGroup.Item>
-//                     ))}
-//                 </ListGroup>
-//             </Card>
-//         </Container>
-//     );
-// }
 
 export default community;
