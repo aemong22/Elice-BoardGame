83c4e9ec367c4a8300305e2cb2ac81b2121b038e
songgaram <pager1436@gmail.com>
2022-05-06 13:03:07 +0900

    feat : commentcard 코드 수정

---
+++ front/src/components/comment/CommentCard.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import {
+    CardActions,
+    CardContent,
+    CardHeader,
+    Typography,
+    Button,
+} from "@mui/material";
+
+function CommentCard({ comment }) {
+    const { user_id: user_name, content } = comment;
+
+    return (
+        <Card sx={{ minWidth: 275 }}>
+            <CardHeader subheader={user_name} />
+            <CardContent>
+                <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                >
+                    {content}
+                </Typography>
+                {isEditable && (
+                    <>
+                        <IconButton
+                            onClick={() => setIsEditing(true)}
+                            sx={{ marginLeft: "auto" }}
+                        >
+                            <EditIcon />
+                        </IconButton>
+                        <IconButton onClick={() => handleDelete()}>
+                            <DeleteIcon />
+                        </IconButton>
+                    </>
+                )}
+            </CardContent>
+        </Card>
+    );
+}
+
+export default CommentCard;
