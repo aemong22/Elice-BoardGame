390aeb7d44734ef83d73d934b712ec5e8b3ae3dd
songgaram <pager1436@gmail.com>
2022-05-06 15:40:33 +0900

    feat : commenteditform 코드 추가

--- front/src/components/comment/CommentEditForm.js
+++ front/src/components/comment/CommentEditForm.js
@@ -1,5 +1,17 @@
+import TextField from "@mui/material/TextField";
+
 function CommentEdidtForm() {
-    return;
+    return (
+        <form onSubmit={handleEdit}>
+            <TextField
+                helperText="Please enter your comment"
+                id="demo-helper-text-aligned"
+                label="Comment"
+            />
+            <MyButton type="submit">완료</MyButton>
+            <MyButton2 onClick={() => navigate(`/community`)}>취소</MyButton2>
+        </form>
+    );
 }
 
 export default CommentEdidtForm;
