7f9d240f14d15cd664a9a7cbf205506563e8c8e5
nowgnas <marshmello.sw@gmail.com>
2022-05-07 20:38:05 +0900

    fix : user_id스키마 추가

--- back/src/db/schemas/communitycontents.js
+++ back/src/db/schemas/communitycontents.js
@@ -3,30 +3,32 @@ import { Schema, model } from "mongoose";
 const CommunityContentSchema = new Schema(
     {
         // content 제목
-        title : {
-            type : String,
-            required : true,
+        title: {
+            type: String,
+            required: true,
         },
         // content 내용
-        content : {
-            type : String,
-            required : true,
+        content: {
+            type: String,
+            required: true,
         },
         // content 작성자
-        author : {
-            type : String,
+        author: {
+            type: String,
+            required: true,
+        },
+        userId: {
+            type: String,
             required: true,
         },
         // comment 한 개시글에 여러개 연결›
         // 배열로 설정하는데 다른스키마와 연결시켜 작성자를 제공해주어야한다.
-        comment :
-            [{type : Schema.Types.ObjectId, ref : "CommunityComment"}],
-        
-    }, 
-    {      
-        timestamps :  true,
+        comment: [{ type: Schema.Types.ObjectId, ref: "CommunityComment" }],
+    },
+    {
+        timestamps: true,
     }
 );
 
-const CommunityContentModel = model("CommunityContent",CommunityContentSchema);
-export {CommunityContentModel};
+const CommunityContentModel = model("CommunityContent", CommunityContentSchema);
+export { CommunityContentModel };
